dbf729599614abc8e7357e9411018f58893491b6
sjshsdjdh152-droid <sjshsdjdh152@gmail.com>
2025-10-22 05:22:58 +0100

    Delete dummy.js

--- dummy.js
+++
@@ -1,8 +0,0 @@
-/*let container = document.createElement("div");
-let menu = document.querySelector(".menu");
-container.appendChild(menu);
-container.className = "container";
-let dummy = document.createElement("div");
-dummy.className = "dummy";
-document.body.appendChild(container);
-document.body.appendChild(dummy);*/
